19245f1e395b3d9800ec70f1ec28697fb58f528f
Ahmed Bay <ahmed-bay@live.fr>
2023-03-21 00:28:05 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,21 +12,21 @@ int main(void)
 	{
 		for (b = 0; b < 100; ++b)
 		{
-				if (a != b  && a < b)
+			if ((a != b) && (a < b))
+			{
+				putchar('0' + a / 10);
+				putchar('0' + a % 10);
+				putchar(' ');
+				putchar('0' + b / 10);
+				putchar('0' + b % 10);
+				if (!(a == 98 && b == 99))
 				{
-						putchar('0' + a / 10);
-						putchar('0' + a % 10);
-						putchar(' ');
-						putchar('0' + b / 10);
-						putchar('0' + b % 10);
-					if (!(a == 98 && b == 99))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
