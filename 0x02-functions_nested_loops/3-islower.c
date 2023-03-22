4a8aef5395e020c84d9e304136a0a6a0b7f43e8d
Ahmed Bay <ahmed-bay@live.fr>
2023-03-22 02:27:46 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islow - a function that checks if a character is lowercase or no.
+ *
+ * @c: is the character to check.
+ *
+ * if "c" is lowercase it returns 1, else it returns 0.
+ *
+ * Return: Always 0
+ */
+int _islow(int c)
+{
+	if ((islower(c))
+			_putchar(48 + 1);
+			else
+			_putchar(48 + 0);
+			return (0);
+			}
+
