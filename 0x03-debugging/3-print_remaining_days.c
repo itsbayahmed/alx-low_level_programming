83feb35890e8e218cb4935866205271f6229979e
Ahmed Bay <ahmed-bay@live.fr>
2023-03-23 16:02:17 +0000

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -21,7 +21,6 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Reamining days: %d\n", 366 - day);
 	}
-	
 	else
 	{
 		if (month == 2 && day == 61)
