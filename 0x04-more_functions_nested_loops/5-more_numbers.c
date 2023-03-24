fa75497da2b61cc519cd6417c6040b830027373e
Ahmed Bay <ahmed-bay@live.fr>
2023-03-24 14:02:26 +0000

    Numbers X 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - A function that prints 10 times
+ * the numbers from 0 to 14.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 11; ++a)
+	{
+		for (b = 0; b < 15; ++b)
+		{
+			if (b < 10)
+			{
+			_putchar('0' + b);
+			}
+			else
+			{
+				_putchar('0' + b / 10);
+				_putchar('0' + b % 10);
+			}
+		_putchar('\n');
+	}
+}
+
+
