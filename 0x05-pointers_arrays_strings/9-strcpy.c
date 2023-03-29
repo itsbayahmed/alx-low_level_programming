010ccfbe5a97d0cef5a17dc51249101b333bef5b
Ahmed Bay <ahmed-bay@live.fr>
2023-03-29 01:06:42 +0000

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ *
+ * @dest: char to check
+ *
+ * @src: char to check
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*src != '\0')
+	{
+		*p++ = *src++;
+	}
+	*p = '\0';
+	return (dest);
+}
