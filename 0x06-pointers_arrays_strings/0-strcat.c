1a47b398c7ba88ed7ec9730790c77ce8a8125d89
Ahmed Bay <ahmed-bay@live.fr>
2023-03-31 00:24:09 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-
-	while (*src)
-	{
-		src++;
-	}
 	while (*dest)
 	{
 		dest++;
 	}
+
+	while (*src)
+	{
 	*dest = *src;
+	src++;
+	dest++;
+	}
 	*dest = '\0';
 	return (dest);
 }
