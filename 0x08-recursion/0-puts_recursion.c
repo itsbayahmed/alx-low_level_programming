7c9d20301a828e336980449cb3e564212f2c7ed9
Ahmed Bay <ahmed-bay@live.fr>
2023-04-06 00:13:53 +0000

    A function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -16,9 +16,11 @@ void _puts_recursion(char *s)
 	if (s[i] != '\0')
 	{
 	_puts_recursion(s);
-	_putchar('\0');
 	}
 	if (s[i] == '\0')
-	_putchar('\n');
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
 }
 
