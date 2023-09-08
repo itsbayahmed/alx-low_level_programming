9de91c4992f38b519f312163982c66dcf04c6b3e
Ahmed Bay <ahmed-bay@live.fr>
2023-09-08 00:49:20 +0000

    I can multiply two numbers!

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: An integer that represent the number of command-line arguments
+ *  passed to the program.
+ * @argv: An array of character pointers that contains the arguments
+ *  passed to the program through the command-line.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int multiply = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			multiply *= atoi(argv[i]);
+		}
+		printf("%d\n", multiply);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
+
+
