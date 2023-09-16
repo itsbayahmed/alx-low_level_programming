08beafa527acd86d6826ea0d2a3b9333113beaac
Ahmed Bay <ahmed-bay@live.fr>
2023-09-16 19:02:50 +0000

    Define a C structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,18 @@
 #ifndef DOG_DOT_H
 #define DOG_DOT_H
 
-struct dog{
+/**
+ * struct dog - a simple C structure.
+ * @name: First member;
+ * @age: Second member;
+ * @owner: Third member;
+ *
+ * Description: This is an example of a C structure
+ *             named dog, and has three members:
+ *             name, age, and owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
