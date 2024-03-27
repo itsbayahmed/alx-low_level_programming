aca551b764b952e94f46a033b126aa1dd4cf504a
itsbayahmed <ahmed-bay@live.fr>
2024-03-27 06:29:44 +0000

    A function that copies a block of memory from one location to another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,9 +16,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	return (src);
 }
