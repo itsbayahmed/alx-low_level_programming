72a70fa64b15046da9e913b5e627c982a6872013
itsbayahmed <ahmed-bay@live.fr>
2024-05-25 20:37:16 +0100

    Calculate and print sum of multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_of_multiples - A function that computes and prints
+ *  the sum of all the multiples of 3 or 5 less than a given number.
+ *
+ *  @n: The given number.
+ *
+ * Return: 0 (Always success)
+ */
+
+int sum_of_multiples(int n)
+{
+	int i = 0;
+	int result = 0;
+
+	while (i < n && ((i % 3 == 0) || (i % 5 == 0)))
+	{
+		result += i;
+		i++;
+	}
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	sum_of_multiples(1024);
+	return (0);
+}
