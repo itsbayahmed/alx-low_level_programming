ee7b3a5cfc9caf78185a6952b9a6005278d0bd8f
itsbayahmed <ahmed-bay@live.fr>
2024-05-26 17:50:44 +0100

    Print the first 50 Fibonacci number

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * fibonacci - A function that generates and prints the Fibonacci
+ *           sequence up to a specified number.
+ *
+ * @n: An integer representing the maximum number
+ *     of Fibonacci numbers to generate.
+ *
+ * Return : Nothing (void).
+ */
+void fibonacci(int n)
+{
+	long i, previous_num, current_num, next_num;
+
+	previous_num = 0;
+	current_num = 1;
+
+	for (i = 2; i <= n; i++)
+	{
+		next_num = previous_num + current_num;
+		printf("%ld", next_num);
+		previous_num = current_num;
+		current_num = next_num;
+		if (i != n)
+			printf(", ");
+	}
+}
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	fibonacci(50);
+	printf("\n");
+	return (0);
+}
+
